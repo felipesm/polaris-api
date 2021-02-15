4ef1518324df8de550e996b742c94d7d368b146d
Felipe Maia <felipe.silwa@gmail.com>
2021-02-15 19:40:00 -0300

    Endpoint para geração do código de barras

--- api.go
+++ api.go
@@ -8,6 +8,7 @@ import (
 func main() {
 
 	http.HandleFunc("/linhadigitavel", getLinhaDigitavel)
+	http.HandleFunc("/codigobarras", getCodigoBarras)
 	porta := ":3000"
 	log.Println("Polaris API iniciada na porta", porta)
 	log.Fatal(http.ListenAndServe(porta, nil))
--- service.go
+++ service.go
@@ -4,45 +4,65 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/polaris-boleto/boletos"
+	errorutil "github.com/polaris-boleto/erro"
 )
 
-// GetCodigoBarras - retorna código de barras
-func GetCodigoBarras(w http.ResponseWriter, r *http.Request) {
+// getCodigoBarras - retorna código de barras
+func getCodigoBarras(w http.ResponseWriter, r *http.Request) {
 
-	// params := r.URL.Query()
+	params := r.URL.Query()
 
-	// codigoBanco := params.Get("codbanco")
-	// valor := params.Get("valor")
-	// vencimento := params.Get("vencimento")
-	// carteira := params.Get("carteira")
-	// nossoNumero := params.Get("numero")
-	// codigobeneficiario := params.Get("codbeneficiario")
+	codigoBarras, erro := gerarCodigoBarras(params)
 
-	// boleto := boletos.InstanciarBoleto(codigoBanco)
+	w.Header().Set("Content-Type", "application/json")
 
+	if erro.Status != 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(erro)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(codigoBarras)
+	}
 }
 
-// gerarLinhaDigitavel - realiza a geração da linha digitavel através da biblioteca polaris
-func gerarLinhaDigitavel(params url.Values) (boletos.LinhaDigitavel, boletos.Erro) {
+func gerarCodigoBarras(params url.Values) (boletos.CodigoBarras, errorutil.Erro) {
 
-	var linha boletos.LinhaDigitavel
-	var erro boletos.Erro
+	var codigoBarras boletos.CodigoBarras
+	var erro errorutil.Erro
 
-	codigoBarras := params.Get("codigobarras")
-	codigoBanco := codigoBarras[0:3]
+	codigoBanco := params.Get("codbanco")
+	agencia, err := strconv.Atoi(params.Get("agencia"))
+	carteira, err := strconv.Atoi(params.Get("carteira"))
+	valor, err := strconv.ParseFloat(params.Get("valor"), 64)
+	vencimento := params.Get("vencimento")
+	nossoNumero := params.Get("numero")
+	codigobeneficiario, err := strconv.Atoi(params.Get("codbeneficiario"))
 
 	boleto, erro := boletos.InstanciarBoleto(codigoBanco)
 
 	if erro.Status != 0 {
-		return linha, erro
+		return codigoBarras, erro
 	}
 
-	return boleto.GetLinhaDigitavel(codigoBarras)
+	if err != nil {
+		return codigoBarras, errorutil.Erro{Titulo: "Dados Inválidos", Mensagem: "Um ou mais dados informados são inválidos. Favor verificar as informações e solicitar novamente.", Status: 400}
+	}
+
+	boleto.SetCodigo()
+	boleto.SetAgencia(int32(agencia))
+	boleto.SetCarteira(int16(carteira))
+	boleto.SetFatorVencimento(vencimento, vencimento == "1990-01-01")
+	boleto.SetValorBoleto(valor, valor == 0)
+	boleto.SetNossoNumero(nossoNumero)
+	boleto.SetCodigoBeneficiario(int32(codigobeneficiario))
+
+	return boleto.GetCodigoBarras()
 }
 
-// GetLinhaDigitavel - retornar linha digitavel
+// getLinhaDigitavel - retornar linha digitavel
 func getLinhaDigitavel(w http.ResponseWriter, r *http.Request) {
 
 	params := r.URL.Query()
@@ -58,4 +78,22 @@ func getLinhaDigitavel(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(linha)
 	}
+}
+
+// gerarLinhaDigitavel - realiza a geração da linha digitavel através da biblioteca polaris
+func gerarLinhaDigitavel(params url.Values) (boletos.LinhaDigitavel, errorutil.Erro) {
+
+	var linha boletos.LinhaDigitavel
+	var erro errorutil.Erro
+
+	codigoBarras := params.Get("codigobarras")
+	codigoBanco := codigoBarras[0:3]
+
+	boleto, erro := boletos.InstanciarBoleto(codigoBanco)
+
+	if erro.Status != 0 {
+		return linha, erro
+	}
+
+	return boleto.GetLinhaDigitavel(codigoBarras)
 }
